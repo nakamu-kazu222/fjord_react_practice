fb2abe3c2502c97333846c93030892ecd8b9fe23
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-12-08 18:46:56 +0900

    メモを記載するコンポーネントを作成した

---
+++ my-memo-app/src/components/WriteMemo.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+const WriteMemo = ({ selectedMemo, onEdit, onRemove }) => {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (selectedMemo) {
+      setText(selectedMemo.text);
+    } else {
+      setText('');
+    }
+  }, [selectedMemo]);
+
+  const handleWrite = () => {
+    if (text.trim() !== '') {
+      if (selectedMemo) {
+        onEdit(selectedMemo.id, text);
+      }
+      setText('');
+    }
+  };
+
+  const handleRemove = () => {
+    if (selectedMemo) {
+      onRemove(selectedMemo.id);
+      setText('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>{selectedMemo ? 'Edit Memo' : 'Add Memo'}</h2>
+      <textarea
+        rows={{ minHeight: '1000px' }}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Write memo"
+      />
+      {selectedMemo && <button onClick={handleWrite}>Save</button>}
+      {selectedMemo && <button onClick={handleRemove}>Remove Memo</button>}
+    </div>
+  );
+};
+
+export default WriteMemo;
