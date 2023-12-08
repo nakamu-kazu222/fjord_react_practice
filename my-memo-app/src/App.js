5ddaf9ac1c5dc0b473e17de07fc652a2c00b6af0
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-12-08 18:47:30 +0900

    メモデータを格納するコンポーネントを作成した

--- my-memo-app/src/App.js
+++ my-memo-app/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MemoList from './components/MemoList';
+import WriteMemo from './components/WriteMemo';
 
-function App() {
+const App = () => {
+  const [memos, setMemos] = useState([]);
+  const [idCounter, setIdCounter] = useState(1);
+  const [selectedMemo, setSelectedMemo] = useState(null);
+
+  const handleNewMemo = () => {
+    const newMemo = {
+      id: idCounter,
+      text: '新規メモ',
+    };
+
+    setMemos([...memos, newMemo]);
+    setIdCounter(idCounter + 1);
+    setSelectedMemo(newMemo);
+  };
+
+
+  const handleWriteMemo = (memoText) => {
+    const newMemo = {
+      id: idCounter,
+      text: memoText,
+    };
+
+    setMemos([...memos, newMemo]);
+    setIdCounter(idCounter + 1);
+    setSelectedMemo(null);
+  }
+
+  const handleEditMemo = (editId, newText) => {
+    const updatedMemos = memos.map((memo) => {
+      if (memo.id === editId) {
+        return { ...memo, text: newText };
+      } else {
+        return memo;
+      }
+    });
+    setMemos(updatedMemos);
+    setSelectedMemo(null);
+  };
+
+  const handleRemoveMemo = (removeId) => {
+    const updatedMemos = memos.filter((memo) => memo.id !== removeId);
+    setMemos(updatedMemos);
+    setSelectedMemo(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleNewMemo}>+</button>
+      <WriteMemo onAdd={handleWriteMemo} selectedMemo={selectedMemo} onEdit={handleEditMemo} onRemove={handleRemoveMemo} />
+      <MemoList memos={memos} onEdit={setSelectedMemo} />
     </div>
   );
-}
+};
 
 export default App;
